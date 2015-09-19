49a249ee9405dad3f24babec35f1c842b140c9c1
iordan93 <iordan93@gmail.com>
2015-09-19 22:28:32 +0300

    Added progress loading, added (almost working) Chrome support

--- WallpaperDownloader.App/WallpaperDownloader.cs
+++ WallpaperDownloader.App/WallpaperDownloader.cs
@@ -5,12 +5,14 @@
     using System.Net;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Interactions;
 
     public class WallpaperDownloader
     {
         public const string WallpapersDirectory = "../../Wallpapers";
 
-        private static FirefoxDriver firefox;
+        private static IWebDriver browser;
         private static WebClient client = new WebClient();
 
         private static string baseUrl;
@@ -19,30 +21,56 @@
 
         public static void Main()
         {
-            var profile = SetupBrowser();
-            firefox = new FirefoxDriver(profile);
-            firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            SetupBrowser();
+            browser.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
 
-            firefox.Navigate().GoToUrl("http://wallpaperswide.com/");
-            string categoryLink = SelectCategory(firefox);
+            browser.Navigate().GoToUrl("http://wallpaperswide.com/");
+            string categoryLink = SelectCategory(browser);
             SelectPages(categoryLink);
             DownloadImages();
-            firefox.Quit();
+            browser.Quit();
         }
 
-        private static FirefoxProfile SetupBrowser()
+        private static void SetupBrowser()
+        {
+            Console.WriteLine("Select a browser to use:{0}1. Mozilla Firefox{0}2. Google Chrome", Environment.NewLine + "  ");
+            int browserChoice = int.Parse(Console.ReadLine());
+            switch (browserChoice)
+            {
+                case 1:
+                    SetupFirefoxBrowser();
+                    break;
+                case 2:
+                    SetupChromeBrowser();
+                    break;
+                default:
+                    Console.WriteLine("Your browser choice is invalid.");
+                    break;
+            }
+
+            Console.Clear();
+        }
+
+        private static void SetupFirefoxBrowser()
         {
             var profile = new FirefoxProfile();
             profile.AcceptUntrustedCertificates = true;
             profile.SetPreference("browser.download.folderList", 2);
             profile.SetPreference("browser.download.dir", Path.GetFullPath(WallpapersDirectory));
             profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "image/jpg,image/jpeg,image/png");
-            return profile;
+            browser = new FirefoxDriver(profile);
         }
 
-        private static string SelectCategory(FirefoxDriver firefox)
+        private static void SetupChromeBrowser()
         {
-            var categories = firefox.FindElementsByCssSelector(".side-panel.categories > li");
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", Path.GetFullPath(WallpapersDirectory));
+            browser = new ChromeDriver(options);
+        }
+
+        private static string SelectCategory(IWebDriver browser)
+        {
+            var categories = browser.FindElements(By.CssSelector(".side-panel.categories > li"));
             Console.WriteLine("Please choose a category:");
             for (int i = 1; i <= categories.Count; i++)
             {
@@ -57,10 +85,10 @@
 
         private static void SelectPages(string categoryLink)
         {
-            firefox.Navigate().GoToUrl(categoryLink);
-            var lastPageElement = firefox.FindElementByCssSelector(".pagination:last-of-type > a:nth-last-child(2)");
+            browser.Navigate().GoToUrl(categoryLink);
+            var lastPageElement = browser.FindElement(By.CssSelector(".pagination:last-of-type > a:nth-last-child(2)"));
             string lastPageUrl = lastPageElement.GetAttribute("href");
-            baseUrl = lastPageUrl.Substring(0, lastPageUrl.LastIndexOf("/"));
+            baseUrl = lastPageUrl.Substring(0, lastPageUrl.LastIndexOf("/") + 1);
             string maxPageString = lastPageElement.Text;
             int maxPage = int.Parse(maxPageString);
 
@@ -81,20 +109,48 @@
 
         private static void DownloadImages()
         {
+            Console.Clear();
             for (int page = startPage; page <= endPage; page++)
             {
-                firefox.Navigate().GoToUrl(baseUrl + "/" + page);
-                var wallpaperPreviewsCount = firefox.FindElementsByCssSelector("li.wall img").Count;
+                browser.Navigate().GoToUrl(baseUrl + page);
+                var wallpaperPreviewsCount = browser.FindElements(By.CssSelector("li.wall img")).Count;
                 for (int currentIndex = 1; currentIndex <= wallpaperPreviewsCount; currentIndex++)
                 {
-                    var wallpaperPreview = firefox.FindElementByCssSelector("li.wall:nth-of-type(" + currentIndex + ") img");
-                    wallpaperPreview.Click();
-                    firefox
-                        .FindElementById("wallpaper-resolutions")
-                        .FindElement(By.CssSelector("a[style]"))
-                        .Click();
-                    firefox.Navigate().Back();
+                    ((IJavaScriptExecutor)browser).ExecuteScript(
+                        "[].forEach.call(document.querySelectorAll(\"#huddown\"), function(el) { el.style.display = \"block\"; })");
+
+                    var wallpaperPreview = browser.FindElement(By.CssSelector("li.wall:nth-of-type(" + currentIndex + ") img"));
+                    var arrow = browser.FindElement(By.CssSelector("li.wall:nth-of-type(" + currentIndex + ") #huddown"));
+                    Actions actions = new Actions(browser);
+                    actions
+                        .MoveToElement(arrow)
+                        .Click()
+                        .Build()
+                        .Perform();
+                    TryDownloadImage();
                 }
+            }
+
+            Console.ResetColor();
+        }
+
+        private static void TryDownloadImage()
+        {
+            try
+            {
+                var frame = browser.FindElement(By.Id("notifyFrame"));
+                browser.SwitchTo().Frame(frame);
+                browser.FindElement(By.Id("dwres_btn")).Click();
+                browser.SwitchTo().DefaultContent();
+                browser.FindElement(By.CssSelector(".ui-icon.bw-icon-b1")).Click();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Image downloaded");
+            }
+            catch (NoSuchElementException)
+            {
+                // It's OK, just skip the image
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Image skipped");
             }
         }
     }
